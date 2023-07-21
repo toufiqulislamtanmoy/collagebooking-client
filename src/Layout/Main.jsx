21e236c674fb991cb532eeb6ae57971061ad8cb7
toufiqulislamtanmoy <tittanmoy@gmail.com>
2023-07-22 02:06:37 +0600

    header and footer done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Shared/Navbar";
+import Footer from "../Pages/Shared/Footer";
 
 
 const Main = () => {
     return (
-        <>
-            <Outlet />
-        </>
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <div className="flex-grow">
+                <Outlet />
+            </div>
+            <Footer />
+        </div>
 
     );
 };
---
+++ src/Pages/Shared/Navbar.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png"
+const Navbar = () => {
+    const navList = <>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/colleges">Colleges</Link></li>
+        <li><Link to="/admission">Admission</Link></li>
+        <li><Link to="/myCollage">My College</Link></li>
+    </>
+    return (
+        <div className="navbar bg-base-200">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {
+                            navList
+                        }
+                    </ul>
+                </div>
+                <Link to="/" className="btn btn-ghost normal-case text-xl">
+                    <img className="h-[60px] w-[100px]" src={logo} alt="" />
+                </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {
+                        navList
+                    }
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <div className="flex gap-2">
+                    <div className="form-control">
+                        <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                    </div>
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60" />
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
